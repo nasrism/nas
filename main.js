5035efdbe7a7fc950a51395461c27df37fc9410b
nasrism <nasrullahismail010@gmail.com>
2023-09-26 08:08:46 +0800

    logo change... and some minor change

--- main.js
+++ main.js
@@ -1,7 +1,85 @@
-let navi = document.querySelector(".top-nav");
+document.addEventListener('click', function (event) {
+  const nav = document.querySelector('.top-nav[data-nav="open"]');
+  if (nav && !nav.contains(event.target)) {
+      nav.setAttribute('data-nav', 'close');
+  }
+});
 
-navbar=()=>{
-  navi.dataset.nav=="close"?navi.dataset.nav="open":navi.dataset.nav="close";
-  console.log(navi.dataset.nav);
-}
+function navbar() {
+  const nav = document.querySelector('.top-nav');
+  const currentNavState = nav.getAttribute('data-nav');
   
+  if (currentNavState === 'open') {
+      nav.setAttribute('data-nav', 'close');
+  } else {
+      nav.setAttribute('data-nav', 'open');
+  }
+}
+
+function navibar() {
+  const nav = document.querySelector('.top-nav');
+  const currentNavState = nav.getAttribute('data-nav');
+  
+  if (currentNavState === 'open') nav.setAttribute('data-nav', 'close');
+}
+
+function scrollToSection(clickedElement) {
+  const targetId = clickedElement.getAttribute('data-target');
+  const targetSection = document.querySelector(targetId);
+  navbar();
+  if (targetSection) {
+    targetSection.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function lazyLoadImages() {
+  const images = document.querySelectorAll('#projects img[data-src]');
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.2, // Adjust this threshold as needed
+  };
+
+  const imageObserver = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+        const src = img.getAttribute('data-src');
+        
+        // Load the image
+        img.src = src;
+        img.removeAttribute('data-src');
+        
+        // Stop observing the image after it's loaded
+        imageObserver.unobserve(img);
+      }
+    });
+  }, options);
+
+  // Start observing each image
+  images.forEach((img) => {
+    imageObserver.observe(img);
+  });
+}
+
+// Call the lazyLoadImages function when the page loads
+window.addEventListener('load', lazyLoadImages);
+
+// project slide
+const project = document.querySelectorAll(".project");
+const slideObserver = new IntersectionObserver(
+  entries=>{
+entries.forEach(entry =>{
+  entry.target.classList.toggle("slide",entry.isIntersecting)
+})},
+{
+  threshold: 0,
+}
+)
+
+project.forEach(project=>{
+  slideObserver.observe(project);
+
+})
+
+// project detail
